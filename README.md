fbdb54c0aae277f79b1204a748bf1dbf013a786d
Thomason, James <JamesThomason@QuickenLoans.com>
2018-07-16 13:58:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Pathfinder README
+
+Pathfinder is a Visual Studio Code extension which improves the built-in Explorer.
+This extension aims to bring over NERDTree functionality to ease developer transition from Vim to VS Code.
+
+## Features
+- Adds a 'pathfinder.showExplorerMenu' command
+  - This command shows a quick pick menu with options to add, move, delete, and copy files.
+- Binds 'pathfinder.showExplorerMenu' to 'm'
+- Rebinds '<enter>' to open files/directories instead of rename them
+
+
+## Extension Settings
+- None yet
+
+
+## Known Issues
+- None... yet?
+
+
+## Release Notes
+### 0.0.1
+Unreleased...
---
+++ src/extension.ts
@@ -0,0 +1,48 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+// @TODO: Look into using a single add command, and determining if the new node is a directory by whether or not it
+// ends in "/"
+const add_file = 'Add a childnode (File)';
+const add_dir = 'Add a childnode (Directory)';
+const rename = 'Move the current node';
+const remove = 'Delete the current node';
+const copy = 'Copy the current node';
+
+export function activate(context: vscode.ExtensionContext) {
+    // @TODO: Look into allowing "m" fire off the "Move the current node" command, instead of having to type "m<enter>"
+    vscode.commands.registerCommand('pathfinder.showExplorerMenu', async () => {
+        let action = await vscode.window.showQuickPick([
+            add_file,
+            add_dir,
+            rename,
+            remove,
+            copy
+        ]);
+
+        if (add_file === action) {
+            vscode.commands.executeCommand('explorer.newFile');
+        }
+        if (add_dir === action) {
+            vscode.commands.executeCommand('explorer.newFolder');
+        }
+        if (rename === action) {
+            vscode.commands.executeCommand('renameFile');
+        }
+        if (remove === action) {
+            // @TODO: Provide a setting that determines to delete or move to trash
+            // vscode.commands.executeCommand('deleteFile');
+            vscode.commands.executeCommand('moveFileToTrash');
+        }
+        if (copy === action) {
+            // @TODO: popup an input to allow user to rename before paste
+            vscode.commands.executeCommand('filesExplorer.copy');
+            vscode.commands.executeCommand('filesExplorer.paste');
+        }
+    });
+}
+
+export function deactivate() {
+    // Should anything go here?
+}
